dd7967108bdd23a0761640e02ea6f6919fb3346f
Sascha Lippert <sascha.lippert@gmail.com>
2020-03-31 16:19:24 +0200

    jar

---
+++ 07_myJar/test/MyJar.java
@@ -0,0 +1,7 @@
+package test;
+
+public class MyJar {
+	public static void main(String[] args) {
+		System.out.println("Hello Test Jar");
+	}
+}
---
+++ 08_myJarUser/user/JarUser.java
@@ -0,0 +1,10 @@
+package user;
+
+import test.*;
+
+public class JarUser {
+	public static void main(String[] args) {
+		System.out.println("Hello Test User");
+		MyJar.main(args);
+	}
+}
