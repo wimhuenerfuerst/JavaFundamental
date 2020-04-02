6094d9e0489d5baabfbd691c59523b656274038f
sascha lippert <sascha.lippert@gmail.com>
2020-04-02 16:08:11 +0200

    switch

---
+++ 18_switch/src/myswitch/MySwtich.java
@@ -0,0 +1,30 @@
+package myswitch;
+
+public class MySwtich {
+
+	public static void main(String[] args) {
+		int i = 2;
+		//int j = 4;
+
+		switch (i) {
+		default:
+			System.out.println("default");
+			break;
+		case 1:
+		case 2:
+			System.out.println("eins-zwei");
+			break;
+		case 3:
+			System.out.println("drei");
+			break;		
+		}
+		
+		//int - Integer
+		//byte - Byte
+		//short - Short
+		//char - Character
+		//String
+		//enum
+	}
+
+}
