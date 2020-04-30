8fc7682d2c7f385f6829b35ee33a3c346076a473
sascha lippert <sascha.lippert@gmail.com>
2020-04-30 15:31:14 +0200

    repo

--- 40_datademo/src/main/java/moviedb/data/MovieRepo.java
+++ 40_datademo/src/main/java/moviedb/data/MovieRepo.java
@@ -5,7 +5,9 @@ import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepo extends JpaRepository<Movie, Long> {
 	Set<Movie> findByGenre(String genre);
 
