5f1cbe559062e56874a98d28f887dbde603b1dc2
sascha lippert <sascha.lippert@gmail.com>
2020-05-05 11:03:19 +0200

    request body

--- 42_bootdemo/src/main/java/de/slippert/bootdemo/controller/HelloWorldController.java
+++ 42_bootdemo/src/main/java/de/slippert/bootdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class HelloWorldController {
 	}
 
 	@PostMapping("/users")
-	public User createUser(User user) {
+	public User createUser(@RequestBody User user) {
 
 		userRepo.save(user);
 
