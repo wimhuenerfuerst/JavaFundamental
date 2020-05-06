a4d428acd21c363d9ac8694e602ded876105ccba
sascha lippert <sascha.lippert@gmail.com>
2020-05-06 16:20:17 +0200

    security

---
+++ 42_bootdemo/src/main/java/de/slippert/bootdemo/security/BasicConfiguration.java
@@ -0,0 +1,27 @@
+package de.slippert.bootdemo.security;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configurable
+@EnableWebSecurity
+public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+		auth.inMemoryAuthentication().withUser("myuser").password(encoder.encode("mypassword")).roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+
+}
